0f5447236c19af4a2480cb8aaede12099151f587
icvitkov <ivana.cvit@hotmail.com>
2019-10-14 17:59:54 +0200

    lession7:keyboard events

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 new Vue({
     el: "#vue-app",
     data: {
-       age: 24,
-       x: 0,
-       y: 0
+    
     },
     methods: {
-      add: function(inc){
-          this.age+= inc;
-      },
-      subtract: function(dec){
-          this.age-=dec;
-      },
-      updateXY: function(event){
-        this.x = event.offsetX;
-        this.y = event.offsetY;
-      },
-      click: function(){
-          alert("You clicked me");
-      }
+     logName: function(){
+       console.log("you entered your name");
+     },
+     logAge: function(){
+       console.log("you entered your age");
+     }
     }
 });
