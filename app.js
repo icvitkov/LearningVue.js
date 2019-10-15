52d8705a7dbe73c59a0465a98e9c37b84d73f4fa
icvitkov <ivana.cvit@hotmail.com>
2019-10-15 02:11:55 +0200

    lesson 11: conditionals

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 new Vue({
-    el: "#vue-app",
-    data: {
-      available: false,
-      nearby: false
-    },
-    methods: {
-  
-    },
-    computed: {
-      compClasses: function(){
-        return {
-          available: this.available,
-          nearby: this.nearby
-        }
-      }
-    }
+  el: "#vue-app",
+  data: {
+    error: false,
+    success: false
+  },
+  methods: {
+
+  },
+  computed: {
+
+  }
 });
