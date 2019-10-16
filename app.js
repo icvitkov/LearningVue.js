a3f19885625be60719b87bad4ed382862be14de9
icvitkov <ivana.cvit@hotmail.com>
2019-10-16 02:24:36 +0200

    lesson 15: Intro to Components

--- app.js
+++ app.js
@@ -1,33 +1,22 @@
-var one = new Vue({
-  el: "#vue-app-one",
-  data: {
-    title: 'Vue App One'
+Vue.component('greeting', {
+  template: '<p>Hey {{name}}, I am a re-usable component. <button @click="changeName"> Change name</button></p>',
+  data: function(){
+    return {
+      name: 'Yoshi'
+    }
   },
   methods: {
-   
-  },
-  computed: {
-    greet: function(){
-      return 'Hello from app one.'
+    changeName: function(){
+      this.name = 'Mario';
     }
   }
+});
+
+var one = new Vue({
+  el: "#vue-app-one",
 });
 
 var two = new Vue({
   el: "#vue-app-two",
-  data: {
-    title: 'Vue App Two'
-  },
-  methods: {
-   changeTitle: function(){
-    one.title = "Title changed";
-  }
-  },
-  computed: {
-    greet: function(){
-      return 'This is app two.'
-    }
-  }
+ 
 });
-
-two.title = "Changed from outside";
